6e6caea5735056466ab9dfd7571eb77ec5dd0e05
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2023-04-11 14:29:17 -0600

    Update main.py

--- Cloud Computing/Code/S3-Lambda-SNS (Object detection & notification)/main.py
+++ Cloud Computing/Code/S3-Lambda-SNS (Object detection & notification)/main.py
@@ -2,7 +2,6 @@
 
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -12,4 +11,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See PyCharm help at https://www.jetbrains.com/help/
